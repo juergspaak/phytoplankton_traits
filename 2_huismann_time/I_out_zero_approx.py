be17f37a7513581decbede6261a1834943c53800
Juerg Spaak <juerg.spaak@gmail.com>
2017-08-11 15:59:22 +0200

    Vectorize I_out

--- 2_huismann_time/I_out_zero_approx.py
+++ 2_huismann_time/I_out_zero_approx.py
@@ -6,128 +6,57 @@ one of the species is near its monocultural equilibrium
 for this call show_I_out()
 """
 import numpy as np
-import generate_communities as com
-import matplotlib.pyplot as plt
-from scipy.integrate import odeint
-from scipy.interpolate import interp1d
-    
-def invader(species, I_T, t, start = 1):
-    """computes the densitiy of the invader species at time t
-    
-    Parameters:
-        species: [invader, resident], two species
-        I_T: list of floats
-        t: array, times at which resident density need to be computed
-        start: float, starting density of invader
-    
-    Returns:
-        W_res: array
-            has the same shape as t, contains the densities of the invader at
-            time t"""
-    W_now = com.equilibrium([I_T[1], I_T[1]], species, True)
-    W_prev = com.equilibrium(I_T[0], species[1], True)
-    l_i, l_r = species[:,-1]
-    a = species[0][0]/species[1][0]*W_now[0]/W_now[1]
-    b = l_i/l_r
-    W_res = resident(species[1], I_T[1], t, W_prev)
-    c_prime = start/W_prev**(a*b)
-    return c_prime*(np.exp((a-1)*l_i*t)*W_res**(a*b))
-    
-def resident(species, I_now, t, start):
-    """ computes the densitiy of the resident species at time t
-    
-    Parameters:
-        species: one species
-        I_now: float, current light
-        t: array, times at which resident density need to be computed
-        start: float, starting density of resident
-    
-    Returns:
-        W_res: array
-            has the same shape as t, contains the densities of the resident at
-            time t"""
-    t = np.array(t)
-    [k,H,p_max,l] = species
-    W_star = p_max/(l*k)*np.log(1+I_now/H) #equilibrium density
-    #exponentially approaches the equilibrial value W_star 
-    return W_star -(W_star-start)*np.exp(-l*t) 
+from scipy.integrate import odeint, simps
+from numerical_r_i import dwdt as dwdt
+import communities_chesson as ches
 
+def r_i(C,E, species,P,carbon, test = False):
+    """computes r_i for the species[:,0] assuming species[:,1] at equilibrium
+    
+    does this by solving the differential equations
+    C should be the density of the resident species, E the incomming light
+    P the period length, carbon = [carbon[0],carbon[1]] the carbon uptake 
+    functions of both species
+    
+    returns r_i for this period"""
+    # first axis is for invader/resident, second for the two species
 
-def invader_growth(biomass,t,species,  I_in, res_fun):
-    """computes the growth rate for the invading species at time t
-    
-    Parameters:
-        biomass: float
-            species density
-        t: float,
-            current time
-        species: [invader, resident]
-            list containing the parameters for two species
-        I_in: float
-            current incoming light
-        res_fun: callable
-            fucntion that returns the resident density at time t"""
-    [k,H,p_max,l] = species[0]
-    #W_res = resident(species[1], I_in, t, start_res)
-    W_res = res_fun(t)
-    return (p_max*np.log((H+I_in)/(H+I_in*np.exp(-species[1][-1]*W_res)))
-            /(species[1][0]*W_res)-l)*biomass
-    
-def resident_growth(biomass,t,species, I_in):
-    """returns the growth of the resident species"""
-    k,H,p_max,l = species[1]
-    dwdt = p_max/k*np.log((H+I_in)/(H+I_in*np.e**(-k*biomass)))-l*biomass
-    return dwdt
-    
+    C = ches.equilibrium(species, carbon, np.random.uniform(50,200))
+    start_dens = np.array([np.ones(C.shape), C])
+    E = np.random.uniform(50,200)
+    t = np.linspace(0,P,50)
+    sol = odeint(dwdt, start_dens.reshape(-1), t,args=(species, E, carbon))
+    sol.shape = len(t),2,2,-1
+    rep = np.random.randint(species.shape[-1])
+    #### Resident check:
+    k,l = species[[0,-1]]
+    equi = com.equilibrium(species, E, "simple")
+    W_rt = equi-(equi-C)*np.exp(-l*t[:, None, None])
+    fig,ax = plt.subplots()
+    plt.plot(t,W_rt[:, 0,rep],'.')
+    plt.plot(t, sol[:, 1,0,rep])
+    plt.plot(t,W_rt[:, 1,rep],'.')
+    plt.plot(t, sol[:, 1,1,rep])
+    ax.set_ylabel("density of resident", fontsize=14)
+    ax.set_xlabel("time", fontsize=14)
+    ### invader:
+    fig,ax = plt.subplots()
+    plt.plot(t,sol[:,0,0,rep])
+    plt.plot(t,sol[:,0,1,rep])
+    ax.set_ylabel("density of invader", fontsize=14)
+    ax.set_xlabel("time", fontsize=14)
+    inv = [0,1]
+    res = [1,0]
 
-def show_I_out():
-    """plots the "exact" computed densities of the invader and the resident
-    species ater a change in incoming light intensity"""
-    #generate random species      
-    species = com.find_species()
-    start_inv = 1#starting density of invading species
-    t_end = 50 #end time of simulation
-    time = np.linspace(0,t_end,300) #time line for integration
-    I_T = np.random.uniform(50,200,2) #light regime [I_prev, I_now]
-    start_res = com.equilibrium(I_T[0], species[1]) #starting density of res.
+    abso = k[inv]*equi[inv]/(k[res]*equi[res])
+    W_it = np.exp((abso[inv]-1)*l[inv]*t[:,None, None])*\
+                  (W_rt[:,res]/C[res])**(abso[inv]*l[inv]/l[res])
+    plt.plot(t, W_it[:,0, rep], '.')
+    plt.plot(t, W_it[:,1, rep], '.')
+        
+    #divide by P, to compare different period length
+    return np.log(sol[1,0,[1,0]]/1)/P #return in different order, because species are swapped
     
-    #approximative solutions
-    approx_inv = np.array(invader(species, I_T, time, start_inv))
-    approx_res = np.array(resident(species[1], I_T[1], time,start_res))
-    
-    #"exact" resident density
-    exact_res = odeint(resident_growth,start_res,time, (species, I_T[1]))
-    exact_res = [e[0] for e in exact_res] #convert to 1dim array
-    #interpolate to solve ode for invader
-    res_fun_prov = interp1d(time, exact_res,'cubic')
-    res_fun = lambda t: res_fun_prov(min(t,50)) #for values above 50
-    #exact solution for invader
-    exact_inv = np.array(odeint(invader_growth,start_inv,time, 
-                                (species, I_T[1],res_fun, 1)))
-    
-    res_fun = lambda t: resident(species[1], I_T[1], t,start_res)
-    
-    #plot figures
-    fig1 = plt.figure(1)
-    plt.plot(time[0::15],approx_res[0::15], '^')
-    plt.plot(time,exact_res)
-    fig1.gca().set_ylabel("density of resident", fontsize=14)
-    fig1.gca().set_xlabel("time", fontsize=14)
-    plt.legend(["approximated", "exact"], loc = "best")
-    
-    fig2 = plt.figure(2)
-    plt.plot(time,[1-exact_res[i]/approx_res[i] for i in range(len(time))],'r')
-    fig2.gca().set_ylabel("relative error, resident", fontsize=14)
-    fig2.gca().set_xlabel("time", fontsize=14)
-    
-    fig3 = plt.figure(3)
-    plt.plot(time[0::15],approx_inv[0::15], '^')
-    plt.plot(time,exact_inv)
-    fig3.gca().set_ylabel("density of invader", fontsize=14)
-    fig3.gca().set_xlabel("time", fontsize=14)
-    plt.legend(["approximated", "exact"], loc = "best")
-    
-    fig4 = plt.figure(4)
-    plt.plot(time,[1-exact_inv[i]/approx_inv[i] for i in range(len(time))],'r')
-    fig4.gca().set_ylabel("relative error, invader", fontsize=14)
-    fig4.gca().set_xlabel("time", fontsize=14)
+spec, carb, I_r = ches.gen_species(ches.sat_carbon_par, num = 5000)
+I = np.random.uniform(50,200, spec.shape[-1])
+r_i(ches.equilibrium(spec, carb, I), I,spec, 25, carb, True)
--- 2_huismann_time/numerical_chesson_terms.py
+++ 2_huismann_time/numerical_chesson_terms.py
@@ -26,9 +26,9 @@ def bound_growth(spec, carbon,I_r ,P):
     species, carbon, I_r should be outputs of gen_species
     I_r should be the range in which I_in fluctuates
     
-    returns deltaC, delta E, r_i = deltaE-deltaC
+    returns ave_C, ave_E, r_i = ave_E-ave_C
     
-    delta E is assumed to be very small"""
+    ave_E is assumed to be very small"""
     if I_r.ndim == 1:
         I_r = np.ones((1,spec.shape[-1]))*I_r[:,np.newaxis]
                        
@@ -92,11 +92,12 @@ def r_i(C,E, species,P,carbon):
     # first axis is for invader/resident, second for the two species
     start_dens = np.array([np.ones(C.shape),C])
     #compute the growth of the two species
-    sol = odeint(dwdt, start_dens.reshape(-1), [0,P], 
-                                (species, E, carbon))
-    sol = sol.reshape(2,2,2,-1) #odeint only alows 1-dim arrays
+    sol = odeint(dwdt, start_dens.reshape(-1), [0,P],args = (species, E, carbon))
+    sol.shape =  2,2,2,-1 #odeint only alows 1-dim arrays        
     #divide by P, to compare different period length
-    return np.log(sol[1,0,:]/1)/P 
+    return np.log(sol[1,0,[1,0]]/1)/P #return in different order, because species are swapped
+
+
         
 def dwdt(W,t,species, I_in,carbon):
     """computes the derivative, either for one species or for two
@@ -132,20 +133,19 @@ def dwdt(W,t,species, I_in,carbon):
     loss = np.array([[l[1],l[0]],[l[0],l[1]]])*W
     reloss = loss.copy()
     reloss[0,0], reloss[1,0] = loss[1,0], loss[0,0]
-    new = rel_abso*growth-reloss
+    new = rel_abso*growth-reloss #rearanging to meet requirements
     dwdt = new.copy()
     dwdt[1,0] = new[0,0]
     dwdt[0,0] = new[1,0]
     dwdt[0,1] = new[0,1]
     return dwdt.reshape(-1)
+    
+if False: #generate species and compute bound growth
+    spec, carb, I_r = ches.gen_species(ches.sat_carbon_par, num = 5000)
+    a,b,c = bound_growth(spec, carb, I_r,50)    
+    
+if False: # check r_i is doing a good job
+    I = np.random.uniform(50,200, spec.shape[-1])
+    r_i(ches.equilibrium(spec, carb, I), I,spec, 10, carb, True)
+    
 
-
-
-counter0 = 0
-spec, carb, I_r = ches.gen_species(ches.sat_carbon_par, num = 5000)
-print(spec.shape)
-
-
-a,b,c = bound_growth(spec, carb, I_r,50)
-print(np.sum(~np.isnan(c)), np.amin(c), "finla")
-plit(*c)
