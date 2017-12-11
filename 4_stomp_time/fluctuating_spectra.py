31f9639530ca25583d99b6adbb72ab0e3528623f
J.W.SPAAK <jurgspaak@unamur.be>
2017-12-11 14:57:25 +0100

    Compute growth over time
    Remove all unnecessary species from the communities
    Compute the growth over fluctuating incoming light to see who dies out

--- 4_stomp_time/fluctuating_spectra.py
+++ 4_stomp_time/fluctuating_spectra.py
@@ -11,6 +11,8 @@ from load_pigments import lambs, dlam
 import multispecies_functions as mf
 from timeit import default_timer as timer
 import matplotlib.pyplot as plt
+from ode_solving import own_ode
+from scipy.integrate import simps
 
 import numpy as np
 
@@ -23,17 +25,24 @@ def gen_com(n_com = 1000):
     l = 0.014*np.random.uniform(1/fac, 1*fac,(r_spec,n_com))
     return np.array([phi,l]), k_spec, alpha
                     
-def I_in_t(t, I_in1, I_in2, period):
+def I_in_t(I_in1, I_in2, period):
     # returns light at given time t, fluctuates between I_in1, I_in2
-    t_rel = (t%period)/period
-    part_1 = 2*np.abs(t_rel-0.5)
-    return part_1*I_in1+(1-part_1)*I_in2
+    def fun(t):
+        t_rel = (t%period)/period
+        part_1 = 2*np.abs(t_rel-0.5)
+        return part_1*I_in1+(1-part_1)*I_in2
+    return fun
+"""
+###############################################################################
+# find potentially interesting communities
 
-# generate species and communities
-par, k_spec, alpha = gen_com(100)
-phi,l = par
 # light regimes
 I_ins = [mf.I_in_def(40/300, 650,50), mf.I_in_def(40/300, 450,50)]
+         
+# generate species and communities
+par, k_spec, alpha = gen_com(1000)
+phi,l = par
+
 # compute the equilibria densities for the different light regimes
 equi = np.empty((len(I_ins),) + phi.shape)
 unfixed = np.empty((len(I_ins),phi.shape[-1]))
@@ -47,9 +56,9 @@ fixed = np.logical_not(np.sum(unfixed, axis = 0))
 surv = equi>0 # species that survived
 # XOR(present in one, present in all)
 change_dom = np.logical_xor(np.sum(surv, axis = 0), np.prod(surv, axis = 0))
-change_dom =change_dom.sum(axis = 0) # at least one species changes
+change_dom = change_dom.sum(axis = 0) # at least one species changes
 
-# throw away uninteressting communities (i.e. not fixed, no change of dominance)
+#throw away uninteressting communities (i.e. not fixed, no change of dominance)
 interesting = np.logical_and(change_dom, fixed)
 
 # throw away communities, that have not been fixed/have no change in dominance
@@ -58,17 +67,76 @@ l = l[..., interesting]
 k_spec = k_spec[...,interesting]
 equi = equi[..., interesting]
 
-# remove all species that did not survive in any of the cases
+# set 0 all species that did not survive in any of the cases
 dead = np.sum(surv[...,interesting], axis = 0)==0
 phi[dead] = 0
 l[dead] = 1 # to aboid division by 0
 k_spec[:,dead] = 0
+
+# maximal richness over all environments in one community
+max_spec = np.amax(np.sum(np.sum(equi>0, axis = 0)>0, axis = 0))
+# sort them accordingly to throw rest away
+com_ax = np.arange(equi.shape[-1])
+spec_sort = np.argsort(np.amax(equi,axis = 0), axis = 0)[-max_spec:]
+equi = equi[np.arange(2).reshape(-1,1,1),spec_sort, com_ax]
+phi = phi[spec_sort, com_ax]         
+l = l[spec_sort, com_ax]
+k_spec = k_spec[np.arange(len(lambs)).reshape(-1,1,1),spec_sort, com_ax]
+"""
+
 # take maximum densitiy over all lights for the starting density of the species
 start_dens = np.amax(equi, axis = 0)
 
+def multi_growth(N,t,I_in, k_spec, phi,l):
+    # sum(N_j*k_j(lambda))
+    tot_abs = np.einsum("sc,lsc->lc", N, k_spec)[:,np.newaxis]
+    # growth part
+    growth = phi*simps(k_spec/tot_abs*(1-np.exp(-tot_abs))\
+                       *I_in(t).reshape(-1,1,1),dx = dlam, axis = 0)
+    return N*(growth-l)
+
+period = 10
+I_in_ref = I_in_t(*I_ins, period)
+a = multi_growth(start_dens,1,I_in_ref,k_spec, phi, l)
+
+# compute 100 periods, 10 timepoints per period
+time = np.linspace(0,100*period,period*100*10)
+
+sol = own_ode(multi_growth,start_dens, time[[0,-1]], args=(I_in_ref, k_spec, phi,l),
+    steps = len(time))
+
+###############################################################################
+# determine extinction prone species
+av_end = np.average(sol[-10:], axis = 0) # average over one period at end
+av_start = np.average(sol[-110:-100], axis = 0) # after finding "equilibrium"
+
+rel_diff = (av_end-av_start)/av_start # relative difference in start and end
+ex_id = np.nanargmin(ex, axis = 0) # species with most difference
+ex_com = ex[ex_id, np.arange(ex.shape[-1])]>0.01
+print(rel_diff[ex_id, np.arange(ex.shape[-1])][:10])
+
+def invasion(res_dens, k_inv, k_spec, I_int, phi_i, l_i):
+    tot_abs = np.einsum("tsc,lsc->tlc", res_dens, k_spec)[:,:,np.newaxis]
+    # growth part
+    growth = phi*simps(k_spec/tot_abs*(1-np.exp(-tot_abs))\
+                       *I_in(t).reshape(-1,1,1),dx = dlam, axis = 0)
+    return N*(growth-l)
 
 
 
 
 
-
+if False:
+    # test whether solving ode was correct
+    def single_growth(N,t,I_in,k_spec, phi, l):
+        tot_abs = np.sum(N*k_spec,axis =1)[:,np.newaxis]
+        growth = phi*simps(k_spec/tot_abs*(1-np.exp(-tot_abs))\
+                           *I_in(t).reshape(-1,1),dx = dlam, axis = 0)
+        return N*(growth-l)
+    i = 10
+    start = timer()
+    sol2 = odeint(single_growth, start_dens[:,i], time, 
+                  args = (I_in_ref, k_spec[...,i], phi[:,i],l[:,i]) )
+    print(timer()-start)
+    plt.plot(time, sol[:,:,i] )
+    plt.plot(time, sol2)
