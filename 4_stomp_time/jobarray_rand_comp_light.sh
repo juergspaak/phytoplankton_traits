4aa91b4505db567747d6294eaee761d717816180
J.W.SPAAK <jurgspaak@unamur.be>
2018-01-10 12:41:18 +0100

    Add composed light case

--- 4_stomp_time/jobarray_rand_comp_light.sh
+++ 4_stomp_time/jobarray_rand_comp_light.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J cont_par
+#SBATCH -J comp_light
 #SBATCH -t 01:05:00
 #SBATCH --mem 100
 #SBATCH -n 1
 #SBATCH -N 1
 
 module load Python/3.5.1-foss-2016a
-python generate_data_continuous_random.py $SLURM_ARRAY_TASK_ID
+python generate_data_comp_light.py $SLURM_ARRAY_TASK_ID
