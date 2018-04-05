c8c87b93f1482a82ecb3e1fa278b84f349adcbc0
J.W.SPAAK <jurgspaak@unamur.be>
2018-04-05 14:29:46 +0200

    Compute richness with real species
    richness_computation changes:
    	Compute the richness of real species
    	Compute pigment richness at beginning and end of experiment
    	Compute EF: Biovolume and pigment concentration
    	Give species identities and not r_pig_spec/r_pig
    
    generate_species:
    	Takes as argument which species are present

--- 3_stomp_time/generate_species.py
+++ 3_stomp_time/generate_species.py
@@ -38,16 +38,29 @@ species_all = pig_spe_id.iloc[np.array(pigment_id_species)]
 species_pigments = species_all.iloc[:,1:].values
 n_diff_spe = species_all.shape[-1]-1
                           
-def gen_com(r_spec, fac, n_com = 100):
+def gen_com(present_species, r_spec, fac, n_com = 100):
+    # check input
+    if r_spec < len(present_species):
+        raise ValueError("`r_spec` must be larger than `present_species`")
+    if len(present_species)>n_diff_spe:
+        raise ValueError("length of `present_species` must be at most"+
+                         "{} entries".format(n_diff_spe))
     # growth parameters of each species
     phi = 2*1e8*np.random.uniform(1/fac, 1*fac,(r_spec,n_com))
     l = 0.014*np.random.uniform(1/fac, 1*fac,(r_spec,n_com))
     
     # assign a species type to each species
-    species_id = np.random.choice(n_diff_spe, (r_spec, n_com))
-    new = species_pigments[:,species_id]
-    alphas = np.random.uniform(1,2,(len(pigments),r_spec,n_com))
+    species_id = np.random.choice(present_species, (r_spec, n_com))
+    # ensure, that each present species occurs at least once
+    species_id[:len(present_species)] = present_species[:,np.newaxis]
+
+    # concentration of each pigment for each species
+    alphas = np.random.uniform(1,2,(len(pigments),r_spec,n_com)) *\
+                species_pigments[:,species_id]
     
-    k_spec = np.einsum("pl,psc->lsc",pigments, alphas*new)
-    k_spec /= np.sum(new,axis = 0)
-    return np.array([phi,l]), k_spec, alphas*new
+    # compute absorption spectrum of each species
+    k_spec = np.einsum("pl,psc->lsc",pigments, alphas)
+    k_spec /= np.sum(alphas,axis = 0) # normalize expected pigment concentration
+    return np.array([phi,l]), k_spec, alphas, species_id
+
+par, k_spec, alphas, species_id = gen_com(np.arange(5),10,4,100)
--- 3_stomp_time/richness_computation.py
+++ 3_stomp_time/richness_computation.py
@@ -7,51 +7,11 @@ light
 import numpy as np
 from scipy.integrate import simps
 
-import load_pigments as lp
 from load_pigments import lambs, dlam
 import multispecies_functions as mf
+from generate_species import gen_com, n_diff_spe
 
 from differential_functions_copy import own_ode
-
-def gen_com(r_pig, r_spec, r_pig_spec, fac,pigs = "real", n_com = 1000):
-    """generates random communities that are inspected for coexistence
-    
-    Parameters:
-    r_pig: int
-        richness of pigments in the regional communitiy
-    r_spec: int
-        richness of species in the regional community
-    r_pig_spec: int
-        richness of pigments in each species
-    fac: float
-        maximal factor by which traits of species differ
-    pigs: "real" or "rand"
-        Determines whether pigments in communities are real pigments or random.
-        In the random case the pigments are the same for all communities
-    n_com: int
-        Number of communities to generate
-    
-    Returns:
-        [phi,l] growth rate parameters of the stomp model
-        k_spec, shape (101,r_spec, n_com)
-            absorption spectrum of all species. Axis 0 is the absoprtion 
-            efficiency frm 400-700 nm
-        alpha: array, shape (r_pig, r_spec, ncom)
-            proportions of pigments in each species""" 
-    # get absorption spectrum of the species
-    if pigs == "rand":
-        k_spec, alpha = mf.spectrum_species(lp.random_pigments(r_pig), 
-                                            r_pig,r_spec,n_com,r_pig_spec)
-    elif pigs == "kuep":
-        k_spec, alpha = mf.spectrum_species(lp.kuepper, 
-                                            r_pig,r_spec,n_com,r_pig_spec)
-    else:
-        k_spec, alpha = mf.spectrum_species(lp.real, 
-                                            r_pig,r_spec,n_com,r_pig_spec)
-    # specific photosynthetic efficiency and loss rate, see Stomp 2004, nature
-    phi = 2*1e8*np.random.uniform(1/fac, 1*fac,(r_spec,n_com))
-    l = 0.014*np.random.uniform(1/fac, 1*fac,(r_spec,n_com))
-    return np.array([phi,l]), k_spec, alpha
                     
 def I_in_t(I_in1, I_in2, period):
     # returns light at given time t, fluctuates between I_in1, I_in2
@@ -61,38 +21,30 @@ def I_in_t(I_in1, I_in2, period):
         return part_1*I_in1+(1-part_1)*I_in2
     return fun
     
-def compute_I_out_intensity(N,I_in,k_spec, time, axis = -1):
-    # computes intensity of outcoming light, integrated over all wave length
-    I_out_data = np.empty((N.shape[0],len(lambs), N.shape[2]))
-    for i in range(len(I_out_data)):
-        tot_abs = np.einsum("sc,lsc->lc", N[i], k_spec)
-        I_out_data[i] = I_in(time[i]).reshape(-1,1)*np.exp(-tot_abs)\
-                        /simps(I_in(time[i]),dx = dlam)
-    intens = simps(I_out_data, dx = dlam, axis = 1)
-    return np.percentile(intens,[5,25,50,75,95], axis = axis)
+def find_survivors(equi, species_id):
+    # compute the average amount of each species in the communities
+    return [np.sum(species_id[equi>0] == i)/equi.shape[-1] 
+                    for i in range(n_diff_spe)]
+
+def pigment_richness(equi, alpha):
+    print(equi.shape, alpha.shape)
+    return np.mean(np.sum(np.sum(equi*alpha, axis = 1)>0, axis = 0))
 
 # standard incoming light fluctuation    
 I_in_ref = I_in_t(mf.I_in_def(40/300,450,50), mf.I_in_def(40/300,650,50),10)
 
-def fluctuating_richness(r_pig = 5, r_spec = 10, r_pig_spec = 3,n_com = 100,
-    fac = 3, l_period = 10, pigs = "real", I_in = I_in_ref,t_const = [0,0.5],
-    randomized_spectra = 0, allow_shortcut = False):
+def fluctuating_richness(present_species = np.arange(5), r_spec = 10,
+    n_com = 100,fac = 3,randomized_spectra = 0, l_period = 10,
+    I_in = I_in_ref, t_const = [0,0.5], allow_shortcut = False):
     """Computes the number of coexisting species
     
     Parameters:
-    r_pig: int
-        richness of pigments in the regional communitiy
     r_spec: int
         richness of species in the regional community
-    r_pig_spec: int
-        richness of pigments in each species
     n_com: int
         Number of communities to generate
     fac: float
         maximal factor by which traits of species differ
-    pigs: "real" or "rand"
-        Determines whether pigments in communities are real pigments or random.
-        In the random case the pigments are the same for all communities
     l_period: float
         Lenght of period of fluctuating incoming light
     I_in: callable
@@ -101,6 +53,9 @@ def fluctuating_richness(r_pig = 5, r_spec = 10, r_pig_spec = 3,n_com = 100,
         Times at which species richness must be computed for constant light
     randomized_spectra: float
         amout by which pigments spectra differ from species
+    allow_shortcut: boolean
+        If True, when for all constant light cases the same species coexist,
+        then fluctuating incoming light is not computed
     
     Returns:
     ret_mat: array, shape (len(t_const)+2, 10)
@@ -118,7 +73,10 @@ def fluctuating_richness(r_pig = 5, r_spec = 10, r_pig_spec = 3,n_com = 100,
     # find potentially interesting communities
              
     # generate species and communities
-    par, k_spec, alpha = gen_com(r_pig, r_spec, r_pig_spec, fac,pigs, n_com)
+    par,k_spec,alpha,species_id = gen_com(present_species, r_spec, fac, n_com)
+    # compute pigment richness at the beginning (the same in all communities)
+    r_pig_start = np.sum(np.sum(alpha[:len(present_species),:,0], axis = 1)>0,
+                         axis = 0)
     if randomized_spectra>0:
         # slightly change the spectra of all species
         # equals interspecific variation of pigments
@@ -139,49 +97,38 @@ def fluctuating_richness(r_pig = 5, r_spec = 10, r_pig_spec = 3,n_com = 100,
     phi = phi[:, fixed]
     l = l[:, fixed]
     k_spec = k_spec[..., fixed]
+    species_id = species_id[...,fixed]
+    alpha = alpha[...,fixed]
 
+    ###########################################################################
+    # return values for constant cases
     # richness in constant lights
-    richness_const = np.sum(equi>0, axis = 1)
-    if allow_shortcut:
-        # find cases, where equilibria species change
-        surv = equi>0 # species that survived
-        # XOR(present in one, present in all)
-        change_dom = np.logical_xor(np.sum(surv, axis = 0), 
-                                    np.prod(surv, axis = 0))
-        change_dom = change_dom.sum(axis = 0)>0 # at least one species changes
-    else:
-        change_dom = np.full(phi.shape[-1], True, dtype = "bool")
-    # outcoming light intensity for the constant light cases
-    intens_I_out = np.full((len(t_const)+2,5),np.nan)
-    intens_I_out[:len(t_const)] = compute_I_out_intensity(equi, I_in, k_spec, 
-                                         np.array(t_const)*l_period).T
-
+    richness_equi = np.zeros(len(t_const)+1)
+    richness_equi[:-1] = np.mean(np.sum(equi>0, axis = 1),axis = 1)
+    
+    surviving_species = np.zeros((len(t_const)+1,n_diff_spe))
+    r_pig_equi = np.zeros(len(t_const)+1)
+    for i in range(len(t_const)):
+        surviving_species[i] = find_survivors(equi[i], species_id)
+        r_pig_equi[i] = pigment_richness(equi[i], alpha)
     # Compute EF, biovolume
-    EF_biovolume = np.full(intens_I_out.shape, np.nan)
+    EF_biovolume = np.full((len(t_const)+1,5),np.nan)
     EF_biovolume[:len(t_const)] = np.percentile(np.sum(equi, axis = 1),
                                 [5,25,50,75,95], axis = -1).T
     
-    # fluctuation is unimportant/didn't change anything
-    if change_dom.sum()==0: 
-        # find number of coexisting species through time
-        richness_fluc,richness_const_max = richness_const[:2]
+    EF_pigment = np.zeros(len(t_const)+1)
+    EF_pigment[:len(t_const)] = np.mean(np.einsum("tsc,psc->tc",equi,alpha),
+                                    axis = 1)
+
     
-        ret_mat = np.array([[(richness==i+1).sum() for i in range(10)] for 
-            richness in [*richness_const, richness_const_max, richness_fluc]])
-        return ret_mat/ret_mat.sum(axis = 1).reshape(-1,1),\
-                intens_I_out , EF_biovolume        
-        
-    # throw away communities, that have no change in dominance
-    phi = phi[...,change_dom]
-    l = l[..., change_dom]
-    k_spec = k_spec[...,change_dom]
-    equi = equi[..., change_dom]
-    
+    ###########################################################################
+    # Prepare computation for fluctuating incoming light
     # set 0 all species that did not survive in any of the cases
-    dead = np.sum((equi>0)[...,change_dom], axis = 0)==0
+    dead = np.sum((equi>0), axis = 0)==0
     phi[dead] = 0
-    l[dead] = 1 # to aboid division by 0
+    l[dead] = 1 # to avoid division by 0
     k_spec[:,dead] = 0
+
     # maximal richness over all environments in one community
     max_spec = np.amax(np.sum(equi>0, axis = 1))
     # sort them accordingly to throw rest away
@@ -191,10 +138,12 @@ def fluctuating_richness(r_pig = 5, r_spec = 10, r_pig_spec = 3,n_com = 100,
     l = l[spec_sort, com_ax]
     equi = equi[np.arange(len(t_const)).reshape(-1,1,1),spec_sort, com_ax]
     k_spec = k_spec[np.arange(len(lambs)).reshape(-1,1,1),spec_sort, com_ax]
+    species_id = species_id[spec_sort, com_ax]
+    alpha = alpha[:,spec_sort, com_ax]
        
     ###########################################################################
-    # take maximum densitiy over all lights for the starting density
-    start_dens = np.amax(equi, axis = 0)
+    # take average densitiy over all lights for the starting density
+    start_dens = np.mean(equi, axis = 0)
 
     def multi_growth(N,t,I_in, k_spec, phi,l):
         # growth rate of the species
@@ -205,7 +154,7 @@ def fluctuating_richness(r_pig = 5, r_spec = 10, r_pig_spec = 3,n_com = 100,
                            *I_in(t).reshape(-1,1,1),dx = dlam, axis = 0)
         return N*(growth-l)
     
-    n_period = 100 # number of periods to simulate
+    n_period = 100 # number of periods to simulate in one run
     
     undone = np.arange(phi.shape[-1])
     # compute 100 periods, 10 timepoints per period
@@ -215,7 +164,7 @@ def fluctuating_richness(r_pig = 5, r_spec = 10, r_pig_spec = 3,n_com = 100,
     sols = np.empty((10,)+phi.shape)
     
     # simulate densities
-    counter = 1
+    counter = 1 # to avoid infinite loops
 
     while len(undone)>0 and counter <1000:
         sol = own_ode(multi_growth,start_dens, time[[0,-1]], 
@@ -243,24 +192,20 @@ def fluctuating_richness(r_pig = 5, r_spec = 10, r_pig_spec = 3,n_com = 100,
 
     #######################################################################
     # preparing return values for richnesses computation
-    
-    intens_I_out[-1] = compute_I_out_intensity(sols,I_in,k_spec, 
-                np.linspace(0,l_period,10), axis = (0,1))  
-    EF_biovolume[1+len(t_const)] = np.percentile(np.sum(sols, axis = (0,1)),
-
-                                [5,25,50,75,95], axis = -1)      
+    EF_biovolume[-1] = np.percentile(np.sum(sols, axis = (0,1)),
+                                [5,25,50,75,95], axis = -1)    
     # find number of coexisting species through time
-    richness_fluc = np.sum(sols[-1]>0,axis = 0)
-    # add the cases where fluctuations didn't change anything
-    richness_fluc = np.append(richness_const[0,~change_dom],richness_fluc)
-    # take the maximal number of coexisting species in each community
-    richness_const_max = np.amax(richness_const, axis = 0)
+    richness_equi[-1] = np.mean(np.sum(sols[-1]>0,axis = 0))
+    surviving_species[-1] = find_survivors(sols[-1], species_id)
+    r_pig_equi[-1] = pigment_richness(sols[-1], alpha)
+    EF_pigment[-1] = np.mean(np.einsum("tsc,psc->c",sols,alpha))/len(sols)
     
-    ret_mat = np.array([[(richness==i+1).sum() for i in range(10)] for 
-        richness in [*richness_const, richness_const_max, richness_fluc]])
-    return ret_mat/ret_mat.sum(axis = 1).reshape(-1,1),intens_I_out, EF_biovolume
+    return (richness_equi, EF_biovolume, r_pig_equi, EF_pigment, r_pig_start,
+            surviving_species)
+            
 
 if __name__ == "__main__":
+    present_species = np.arange(5)
     r_pig = 5
     r_spec = 10
     r_pig_spec = 3
