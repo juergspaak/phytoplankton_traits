Correct units
Use consistent units in programming

generate_species.py
@@ -35,8 +35,8 @@ species_pigments[np.isnan(species_pigments)] = 0
 # minus 2, because of pigment labeling and whether pigments are found
 n_diff_spe = len(pigments)
                          
-def gen_com(present_species, fac, n_com_org = 100, I_ins = None, k_BG = None,
-            run = 0):
+def gen_com(present_species, fac, n_com_org = 100, I_ins = None, 
+            k_BG = None, zm = 1, run = 0):
     """Generate random species
     
     Generate species with random absorption spectrum according to the table
@@ -60,7 +60,13 @@ def gen_com(present_species, fac, n_com_org = 100, I_ins = None, k_BG = None,
         k_spec: array with shape (len(lambs), len(present_species),n_com)
             The absorption spectrum of each species in each community
         alphas: array with shape (len(pigments), len(present_species),n_com)
-            Concentration of each pigment for each species in each community"""
+            Concentration of each pigment for each species in each community
+            
+    units of the returned variables are:
+        [phi]: fl * (mu mol photons)^-1
+        [l]: h^-1
+        [k_spec]: cm^2 * fl^-1
+        [alphas]: unitless"""
     if run == 2:
         return [None, None], None, None, False
     # internally generate to many species, as some will not survive
@@ -76,32 +82,37 @@ def gen_com(present_species, fac, n_com_org = 100, I_ins = None, k_BG = None,
                          "{} entries".format(n_diff_spe))
     
     # photosynthetic efficiency for each species
-    phi = 2*1e6*np.random.uniform(1/fac, 1*fac,(r_spec,n_com))
+    # unit: [phi] = fl * (mumol photons)^-1
+    phi = 2.0*1e6*np.random.uniform(1/fac, 1*fac,(r_spec,n_com))
     # loss rate of the community
+    # unit: [l] = h^-1
     l = 0.014*np.random.uniform(1/fac, 1*fac,(n_com))
 
     # concentration of each pigment for each species
+    # unit: [alphas] = unitless
     alphas = np.random.uniform(1,2,(len(pigments),r_spec,n_com)) *\
                 species_pigments[:,present_species, np.newaxis]
 
     # compute absorption spectrum of each species
+    # unit: [k_spec] = cm^2 * fl^-1
     k_spec = np.einsum("pl,psc->lsc",pigments, alphas)
     
     # Total absorption of each species should be equal (similar to Stomp)
-    tot_abs = np.mean(simps(k_spec, dx = lp.dlam, axis = 0), axis = -1)
-    k_spec = k_spec/tot_abs[:,np.newaxis]*2.0e-7
+    int_abs = np.mean(simps(k_spec, dx = lp.dlam, axis = 0), axis = -1)
+    k_spec = k_spec/int_abs[:,np.newaxis]*2.0e-7
 
     # change pigment concentrations accordingly
-    alphas = alphas/tot_abs[:,np.newaxis]*2.0e-7
+    alphas = alphas/int_abs[:,np.newaxis]*2.0e-7
     
     # check survivability in monoculture
     if not(I_ins is None):
-        surv = mono_culture_survive(phi/l,k_spec, I_ins,k_BG)
+        surv = mono_culture_survive(phi/l,k_spec, I_ins,k_BG,zm)
         n_surv = min(n_com_org, *np.sum(surv, axis = -1))
         
         # in some unprobable cases this might generate less than n_com species
         if n_surv == 0:
-            return gen_com(present_species, fac, n_com_org, I_ins, k_BG, run+1)
+            return gen_com(present_species, fac, n_com_org, I_ins, k_BG, zm,
+                           run+1)
             
         spec_id = np.argsort(1-surv,axis = 1)[:,:n_surv]
         dummy_id = np.arange(r_spec).reshape(-1,1)
@@ -118,7 +129,7 @@ def gen_com(present_species, fac, n_com_org = 100, I_ins = None, k_BG = None,
     
     return np.array([phi,l]), k_spec, alphas, True
 
-def mono_culture_survive(par, k_spec, I_ins, k_BG = None):
+def mono_culture_survive(par, k_spec, I_ins, k_BG = None,zm = 1):
     """check whether each species could survive in monoculture
     
     par: phi/l
@@ -129,10 +140,7 @@ def mono_culture_survive(par, k_spec, I_ins, k_BG = None):
         Surv: boolean array with same shape as par, indicating which species
         survive in all light conditions"""
     # light condition
-    if k_BG is None:
-        light = I_ins.view()
-    else:
-        light = I_ins*(1-np.exp(-k_BG))/k_BG
+    light = np.where(k_BG ==0, I_ins, I_ins*(1-np.exp(-k_BG*zm))/(k_BG*zm))
     light.shape = -1,len(lp.lambs),1,1
     # initial growth rate
     init_growth = par*simps(light*k_spec,dx = lp.dlam,axis = 1)-1
@@ -153,6 +161,6 @@ if __name__ == "__main__":
     
     # plot the absorption spectrum of random species
     plt.figure()
-    par, k_spec, alphas = gen_com(np.random.randint(11,size = 5),4,100,
+    par, k_spec, alphas, a  = gen_com(np.random.randint(11,size = 5),4,100,
                         50*I_inf.sun_spectrum["blue sky"], I_inf.k_BG["ocean"])
     plt.plot(k_spec[...,0])   

richness_computation.py
@@ -25,7 +25,7 @@ def pigment_richness(equi, alpha):
     return np.mean(np.sum(np.sum(equi*alpha, axis = -2)>0, axis = -2),-1)
 
 def multispecies_equi(fitness, k_spec, I_in = 50*I_in.sun_spectrum["blue sky"],
-                      runs = 5000, k_BG = 0):
+                      k_BG = np.array([0]), zm = 1, runs = 5000):
     """Compute the equilibrium density for several species with its pigments
     
     Compute the equilibrium density for the species with the parameters
@@ -51,8 +51,7 @@ def multispecies_equi(fitness, k_spec, I_in = 50*I_in.sun_spectrum["blue sky"],
     unfixed: array, (shape = n)
         Boolean array indicating in which communitiy an equilibrium was found
     """
-    k_BG = np.asarray(k_BG)
-    k_BG.shape = -1,1
+    k_BG = k_BG.reshape(-1,1)
     # starting densities for iteration, shape = (npi, itera)
     equis = np.full(fitness.shape, 1e7) # start of iteration
     equis_fix = np.zeros(equis.shape)
@@ -63,8 +62,9 @@ def multispecies_equi(fitness, k_spec, I_in = 50*I_in.sun_spectrum["blue sky"],
     unfixed = np.full(fitness.shape[-1], True, dtype = bool)
     n = 20
     i = 0
+    
     #print(equis.shape, equis_fix.shape, fitness.shape, np.sum(unfixed), abs_points.shape)
-    while np.sum(unfixed)/equis.shape[-1]>0.01 and i<runs:          
+    while np.any(unfixed) and i<runs:          
         # sum_i(N_i*sum_j(a_ij*k_j(lam)), shape = (len(lam),itera)
         tot_abs = np.einsum('ni,lni->li', equis, abs_points) + k_BG
         # N_j*k_j(lam), shape = (npi, len(lam), itera)

sim_EF_time.py
@@ -60,9 +60,10 @@ def find_EF(present_species, n_com, sky, lux, envi):
     """
     k_BG = I_inf.k_BG[envi]
     k_BG.shape = -1,1,1
+    zm = I_inf.zm[envi]
     # generate species
     [phi,l], k_spec, alpha, feasible = gen_com(present_species,2, n_com,
-                I_ins = np.array([lux*sun_spectrum[sky]]),k_BG = k_BG)
+                I_ins = np.array([lux*sun_spectrum[sky]]),k_BG = k_BG, zm = zm)
     
     if not feasible:
         return np.full((5,len(time)+1),np.nan)
@@ -71,15 +72,15 @@ def find_EF(present_species, n_com, sky, lux, envi):
     r_spec = len(present_species)
     # incoming light regime
     I_in = lambda t: lux*sun_spectrum[sky]
+
     # compute equilibrium densities
-    equi = rc.multispecies_equi(phi/l, k_spec, I_in(0))[0]
+    equi = rc.multispecies_equi(phi/l, k_spec, I_in(0), k_BG, zm)[0]
     # when species can't survive equi returns nan
     equi[np.isnan(equi)] = 0
     equi.shape = 1,*equi.shape
     
     # starting density
     start_dens = np.full(equi.shape, 1e7)/r_spec
-    
     # compute densities over time
     def multi_growth(N_r,t):
         # compute the growth rate of the species at densities N_r and time t
@@ -88,11 +89,11 @@ def find_EF(present_species, n_com, sky, lux, envi):
         N = N_r.reshape(-1,n_com)
         # growth rate of the species
         # sum(N_j*k_j(lambda))
-        tot_abs = np.nansum(N*k_spec, axis = 1, keepdims = True)# + zm*k_BG
+        tot_abs = zm*(np.nansum(N*k_spec, axis = 1, keepdims = True) + k_BG)
         # growth part
         growth = phi*simps(k_spec/tot_abs*(1-np.exp(-tot_abs))\
                            *I_in(t).reshape(-1,1,1),dx = dlam, axis = 0)
-        
+    
         return (N*(growth-l)).flatten() # flatten for odeint
         
     sol_ode = odeint(multi_growth, start_dens.reshape(-1), time)
@@ -117,8 +118,7 @@ def find_EF(present_species, n_com, sky, lux, envi):
     fitness = phi/l
     fitness_t = [np.nanmean(fitness[d>=start_dens[0]]) for d in dens]
     return EF_mean, EF_var, r_pig, r_spec, fitness_t
-
-
+ 
 iters = 5000
 n_com = 100
 r_specs = np.random.randint(1,15,iters) # richness of species
@@ -145,8 +145,7 @@ environments = environments[np.random.randint(len(environments), size = iters)]
 
 columns = ["species","r_spec", "sky", "lux", "envi"] + EF_cols + r_pig_cols + \
             r_spec_cols + var_cols + fit_cols
-            
-            
+                       
 data = pd.DataFrame(None, columns = columns, index = range(iters))
 
 i = 0