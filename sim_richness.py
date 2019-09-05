49570796cc4aa126857287960da1a0a24dc469e6
jurg.spaak@unamur.be <jurg.spaak@unamur.be>
2019-09-05 11:27:33 +0200

    Compute ND per community and return

--- sim_richness.py
+++ sim_richness.py
@@ -28,7 +28,7 @@ save_string = "data/data_richness"+str(save)+".csv"
 
 start = timer()
 iters = 10000 # number of random settings
-n_com = 100 # number of communities in each setting
+n_com = 10 # number of communities in each setting
 
 
 r_specs = np.random.randint(1,16,iters) # richness of species
@@ -44,10 +44,13 @@ cases = ["Const1", "Const2", "Const3", "Const4", "Fluctuating"]
 
 prob_cols = ["spec_rich,{}".format(i) for i in range(6)] \
             + ["pig_rich,{}".format(i) for i in range(len(pigments)+1)]
+            
+NFD_cols = ["ND,{}".format(i) for i in range(1,6)] \
+            +["FD,{}".format(i) for i in range(1,6)] + ["no_NFD"]
 
 EF_cols = ["biovolume,{}".format(i) for i in ["05",25,50, 75, 95]]
 columns = ["case","species","fac","period","lux1", "lux2", "r_pig_start",
-           "r_pig_equi", "r_spec_equi"]+EF_cols+ prob_cols +["n_fix"]
+           "r_pig_equi", "r_spec_equi"] +NFD_cols+EF_cols+ prob_cols +["n_fix"]
 
 data = pd.DataFrame(None,columns = columns, index = range(len(cases)*iters))
     
@@ -64,7 +67,8 @@ while timer()-start <3600-(time_for_10):
     present_species = np.random.choice(n_diff_spe, r_specs[i],replace = True)
     # compute the richnesses
     (richness_equi, EF_biovolume, r_pig_equi, r_pig_start, prob_spec, 
-            prob_pig, n_fix) = rc.fluctuating_richness(present_species, 
+            prob_pig, n_fix, ND, FD, no_NFD) = rc.fluctuating_richness(
+            present_species, 
             n_com , facs[i], periods[i],I_in,np.linspace(0,0.5,4))
     print(i, "iteration")
     try:
@@ -72,7 +76,8 @@ while timer()-start <3600-(time_for_10):
         for k,case in enumerate(cases):
             data.iloc[len(cases)*i+k] = [cases[k],str(present_species), facs[i], 
                 periods[i], *luxs[i], r_pig_start, r_pig_equi[k],richness_equi[k],
-                *EF_biovolume[k], *prob_spec[k], *prob_pig[k], n_fix]
+                *ND[k], *FD[k], no_NFD[k], *EF_biovolume[k], *prob_spec[k],
+                *prob_pig[k], n_fix]
     except TypeError:
         pass # system was no feasible
     i+=1
